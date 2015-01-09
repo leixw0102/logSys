d58b1d297f813bc3cc0e911a0c9ecbb80a8d6737
leixw0102 <0102lei@163.com>
2015-01-09 15:48:57 +0800

    add replay

--- stb-parser/src/main/java/tv/icntv/log/stb/replay/ReplayJob.java
+++ stb-parser/src/main/java/tv/icntv/log/stb/replay/ReplayJob.java
@@ -14,6 +14,15 @@ package tv.icntv.log.stb.replay;/*
  *      limitations under the License.
  */
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.ToolRunner;
 import tv.icntv.log.stb.core.AbstractJob;
 
 import java.util.Map;
@@ -32,4 +41,26 @@ public class ReplayJob extends AbstractJob {
         return true;
     }
 
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration configuration = super.getConf();
+        configuration.set("mapreduce.output.fileoutputformat.compress","true");
+        configuration.set("mapreduce.output.fileoutputformat.compress.codec","com.hadoop.compression.lzo.LzopCodec");
+        configuration.set("mapreduce.map.output.compress","true");
+        configuration.set("mapreduce.map.output.compress.codec","com.hadoop.compression.lzo.LzopCodec");
+        Job replay = Job.getInstance(configuration);
+        replay.setJarByClass(getClass());
+        replay.setMapperClass(ReplayMapper.class);
+        replay.setOutputKeyClass(NullWritable.class);
+        replay.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(replay, new Path(args[0]));
+        FileOutputFormat.setOutputPath(replay, new Path(args[1]));
+        replay.setNumReduceTasks(0);
+        return replay.waitForCompletion(true)?0:1;
+    }
+    public static void main(String[]args) throws Exception {
+        Configuration configuration= new Configuration();
+        int i= ToolRunner.run(configuration, new ReplayJob(), args);
+        System.exit(i);
+    }
 }
